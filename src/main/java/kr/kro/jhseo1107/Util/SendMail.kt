444f0bf63e7ea0fbf4f69c594d100ae4568fc072
Janghyub Seo <seojanghyeob@gmail.com>
2020-07-30 20:14:46 +0900

    Some Mailing

--- src/main/java/kr/kro/jhseo1107/Util/SendMail.kt
+++ src/main/java/kr/kro/jhseo1107/Util/SendMail.kt
@@ -4,6 +4,7 @@ import java.util.Properties
 import javax.mail.*
 import javax.mail.internet.InternetAddress
 import javax.mail.internet.MimeMessage
+import javax.mail.Authenticator
 
 fun SendMail (sender: String, reciever: String, content: String){
     var from = sender
@@ -12,12 +13,12 @@ fun SendMail (sender: String, reciever: String, content: String){
 
     var properties:Properties = System.getProperties()
     properties.put("mail.smtp.host", host)
-    properties.put("mail.smtp.port", 465)
-    properties.put("mail.smtp.ssl.enable", true)
-    properties.put("mail.smtp.auth", true)
+    properties.put("mail.smtp.port", "587")
+    properties.put("mail.smtp.starttls.enable", "true")
+    properties.put("mail.smtp.auth", "true")
 
     var session = Session.getInstance(properties, object: Authenticator() {
-       override fun getPasswordAuthentication(): PasswordAuthentication
+       protected override fun getPasswordAuthentication(): PasswordAuthentication
        {
            return PasswordAuthentication(from, TomcatProperties.EmailPassword())
        }
@@ -25,11 +26,17 @@ fun SendMail (sender: String, reciever: String, content: String){
 
     session.debug = true
 
-    var message =  MimeMessage(session)
-    message.setFrom(InternetAddress(from))
-    message.addRecipient(Message.RecipientType.TO, InternetAddress(to))
-    message.setSubject("Uptune Register Verification")
-    message.setText("Your 6-digit code is "+content+".")
+    try {
+        var message = MimeMessage(session)
+        message.setFrom(InternetAddress(from))
+        message.addRecipient(Message.RecipientType.TO, InternetAddress(to))
+        message.setSubject("업튠 가입을 환영합니다!")
+        message.setText("저희 업튠을 이용해주셔서 감사합니다! 입력해야 하는 보안 코드는 " + content + "입니다.")
 
-    Transport.send(message)
+        Transport.send(message)
+    }
+    catch (e: Exception)
+    {
+        e.printStackTrace();
+    }
 }
